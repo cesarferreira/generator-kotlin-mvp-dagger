2fc57dc987cc4c1b9f39484a5aef815786411f8e
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2018-02-04 01:46:24 +0000

    refactoring

---
+++ app/src/main/java/org/cesarferreira/kotlinstarterkit/data/entities/MovieDetailsEntity.kt
@@ -0,0 +1,9 @@
+package org.cesarferreira.kotlinstarterkit.data.entities
+
+data class MovieDetailsEntity(val id: String?,
+                              val title: String?,
+                              val year: String?,
+                              val cast: String?,
+                              val director: String?,
+                              val summary: String?,
+                              val poster: String?)
